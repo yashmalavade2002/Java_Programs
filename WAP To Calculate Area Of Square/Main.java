3dddb88559ac18e5a307a7cd094d096f58548a7b
Yash Malvade <yashmalvade91@gmail.com>
2025-01-01 13:21:21 +0530

    Main.java

---
+++ WAP To Calculate Area Of Square/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+      int side;
+      side=4;
+    int  area =side*side;
+      System.out.println("Area Of Square is : "+area);
+    }
+}
