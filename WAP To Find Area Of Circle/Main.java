eb02c07083983d1ead41cd1202825979132ee09e
Yash Malvade <yashmalvade91@gmail.com>
2025-01-01 13:14:51 +0530

    Create Main.java

---
+++ WAP To Find Area Of Circle/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+      final double PI=3.14;
+      int r=4;
+      Double area =PI*r*r;
+      System.out.println("Area Of Circle is : "+area);
+    }
+}
