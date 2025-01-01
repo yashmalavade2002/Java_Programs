54d79652a4e39865a03fce34b251b536b0855a5d
Yash Malvade <yashmalvade91@gmail.com>
2025-01-01 17:44:46 +0530

    Update Main.java

--- WAP To Swap 2 Nos Without Using Temporary Variable/Main.java
+++ WAP To Swap 2 Nos Without Using Temporary Variable/Main.java
@@ -14,7 +14,7 @@ public class Main {
   y = x - y;
   x = x - y;
 
-  System.out.println("Before Swapping");
+  System.out.println("After Swapping");
   System.out.println("Value of x is :" + x);
   System.out.println("Value of y is :" + y);
  }
