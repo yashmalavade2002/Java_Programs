50def8bb5299d5d14d0cb3c5e33af7bc7cd9064e
Yash Malvade <yashmalvade91@gmail.com>
2025-01-01 13:08:29 +0530

    Main.java

---
+++ WAP To Find Area Of Rectangle/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+      int area,length,width;
+      length=10;
+      width=2;
+      area=length*width;
+      System.out.println("Area Of Rectangle is : "+area);
+    }
+}
