0dff369b51d9110bd0bd4f4da19221ba890e9e1a
Yash Malvade <yashmalvade91@gmail.com>
2025-01-01 16:29:34 +0530

    Create Main.java

---
+++ WAP To Check Given No is Even or Odd/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter The No");
+        Scanner r=new Scanner(System.in);
+        n=r.nextInt();
+        if(n%2==0){
+             System.out.println("Even No !!");
+        }
+             else 
+             {
+                  System.out.println("odd No !!");
+                 
+             }
+    
+    }
+}
