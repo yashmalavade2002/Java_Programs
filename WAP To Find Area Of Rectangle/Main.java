ee650262d2dc8546435be29a5d706927fadc218f
Yash Malvade <yashmalvade91@gmail.com>
2025-02-10 20:36:03 +0530

    Delete WAP To Find Area Of Rectangle directory

--- WAP To Find Area Of Rectangle/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-      int area,length,width;
-      length=10;
-      width=2;
-      area=length*width;
-      System.out.println("Area Of Rectangle is : "+area);
-    }
-}
