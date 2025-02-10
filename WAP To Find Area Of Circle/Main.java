cd129010638203dace98d268223fb5c8bd382b61
Yash Malvade <yashmalvade91@gmail.com>
2025-02-10 20:35:07 +0530

    Delete WAP To Find Area Of Circle directory

--- WAP To Find Area Of Circle/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-      final double PI=3.14;
-      int r=4;
-      Double area =PI*r*r;
-      System.out.println("Area Of Circle is : "+area);
-    }
-}
