c4dd3f9420f932bf719e8e1cb71b9a0bcd6a52ec
Yash Malvade <yashmalvade91@gmail.com>
2025-02-10 20:35:46 +0530

    Delete WAP To Calculate Area Of Triangle directory

--- WAP To Calculate Area Of Triangle/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-      double base,height;
-      base=5;
-      height=10;
-    double  area =0.5*base*height;
-      System.out.println("Area Of Triangle is : "+area);
-    }
-}
