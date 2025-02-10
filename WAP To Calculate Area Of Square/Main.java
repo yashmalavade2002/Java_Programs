66a0cebd43d73657fa1b62ef2e79e5c25e15febe
Yash Malvade <yashmalvade91@gmail.com>
2025-02-10 20:35:33 +0530

    Delete WAP To Calculate Area Of Square directory

--- WAP To Calculate Area Of Square/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-      int side;
-      side=4;
-    int  area =side*side;
-      System.out.println("Area Of Square is : "+area);
-    }
-}
