3dbe460300058968296f77d5097bbbd10d02637c
Yash Malvade <yashmalvade91@gmail.com>
2025-02-26 01:36:55 +0530

    Update ReverseString.java

--- Strings Program/ReverseString.java
+++ Strings Program/ReverseString.java
@@ -1,24 +1,16 @@
-// public class Main {
-//     public static void main(String[] args) {
-//     	String str = "hello";
-//     	String reverseStr = "";
-//     	char ch;
-//     	for(int i =0;i<str.length();i++) {
-//     		ch = str.charAt(i);
-//     		reverseStr = ch + reverseStr ;
+public class Main {
+    public static void main(String[] args) {
+    	String str = "hello";
+    	String reverseStr = "";
+    	char ch;
+    	for(int i =0;i<str.length();i++) {
+    		ch = str.charAt(i);
+    		reverseStr = ch + reverseStr ;
     		
-//     	}
-//     	System.out.println("Reverse String is "+reverseStr);
+    	}
+    	System.out.println("Reverse String is "+reverseStr);
     	
-//     }
-// }
+    }
+}
 
-// Approch 2
-// public class Main {
-//     public static void main(String[] args) {
-//         String str = "hello";
-//         StringBuilder reverseStr = new StringBuilder(str).reverse(); // Efficient reversal
-//         System.out.println("Reverse String is " + reverseStr);
-//     }
-// }
 
