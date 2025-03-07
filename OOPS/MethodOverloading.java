2e622ba74ce4310b68bdc0e950539a6e0dd2f482
Yash Malvade <yashmalvade91@gmail.com>
2025-03-07 21:00:28 +0530

    Create MethodOverloading.java

---
+++ OOPS/MethodOverloading.java
@@ -0,0 +1,27 @@
+/* Method Overloading   */
+class A {
+    void add() {
+        int a = 10, b = 20, c;
+        c = a + b;
+        System.out.println(c);
+    }
+
+    void add(int x, int y) {
+        int c;
+        c = x + y;
+        System.out.println(c);
+    }
+
+    void add(int x, double y) {
+        double c;
+        c = x + y;
+        System.out.println(c);
+    }
+
+    public static void main(String[] args) {
+        A r = new A();
+        r.add();
+        r.add(100, 200);
+        r.add(50, 45.90);
+    }
+}
