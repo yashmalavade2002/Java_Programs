c64949b141d04333f29a848fb6f556e88a82ad71
Yash Malvade <yashmalvade91@gmail.com>
2025-03-07 21:30:07 +0530

    Create MethodOverriding.java

---
+++ OOPS/MethodOverriding.java
@@ -0,0 +1,20 @@
+/* Method Overriding */
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myAnimal = new Dog(); // Upcasting
+        myAnimal.makeSound(); // Output: "Dog barks"
+    }
+}
