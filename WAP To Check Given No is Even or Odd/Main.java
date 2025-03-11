90ce269027e2233446e1d4c938cddae376dadda9
Yash Malvade <yashmalvade91@gmail.com>
2025-03-11 16:18:03 +0530

    Update Main.java

--- WAP To Check Given No is Even or Odd/Main.java
+++ WAP To Check Given No is Even or Odd/Main.java
@@ -16,3 +16,23 @@ class Main {
     
     }
 }
+// Print Even and Odd Numbers from 1 to N
+public class Main {
+
+    public static void main(String[] args) {
+        int N = 20;
+        System.out.println("Even Nos Are : ");
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Odd Nos Are : ");
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
