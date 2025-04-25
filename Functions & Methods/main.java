9baf947d1038e1e514373b489c48678009040b94
Yash Malvade <yashmalvade91@gmail.com>
2025-04-25 18:14:04 +0530

    Update main.java

--- Functions & Methods/main.java
+++ Functions & Methods/main.java
@@ -17,3 +17,31 @@ public class Functions {
     }
 
 }
+
+
+// Methods
+import java.util.Scanner;
+ // user defined methods
+class Algebra{
+    int add(int a ,int b){
+        int ans = a+b;
+        return ans;
+    }
+}
+
+public class Functions {
+public static void main(String[] args) {
+    Algebra obj = new Algebra();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter nos ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println("sum of input nos is ");
+  int  ans=obj.add(a, b);
+  System.out.println(ans);
+
+
+}
+    
+}
+
